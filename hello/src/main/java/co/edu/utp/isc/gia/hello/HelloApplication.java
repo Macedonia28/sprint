d05e8ac2a1772803b3fa32bf1db5d5423f4f6bef
alejandro <alejoagudelo23@gmail.com>
2020-03-30 21:31:49 -0500

    se sube el proyecto hello con el metodo hello

---
+++ hello/src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
@@ -0,0 +1,27 @@
+package co.edu.utp.isc.gia.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("hello")
+
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+
+    }
+
+    @GetMapping("/{nombre}/{apellido}")
+    public String Hello(
+            @PathVariable("nombre") String nombre,
+            @PathVariable("apellido") String apellido) {
+        return String.format("hello %s %s", nombre, apellido);
+    }
+}
